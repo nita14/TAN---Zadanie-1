5b6e44786e09f93ddd2ffe40aad74dcbb28d242e
Adam nicinski <adamnicinski@hotmail.com>
2023-01-03 20:38:16 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebCrawlerAN
+    {
+        class Program
+        {
+            static async Task Main(string[] args)
+            {
+                string websiteUrl = "https://pja.edu.pl/";
+                HttpClient httpclient = new HttpClient();
+                HttpResponseMessage response = await httpclient.GetAsync(websiteUrl);
+
+                string content = await response.Content.ReadAsStringAsync();
+
+                //adresy e-mail i numery telefonu
+
+                MatchCollection matchCollection = Regex.Matches(content, "[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-z][a-z]|[0-9- ][a-zA-Z0-9]+[@][a-zA-Z]+[.][a-z][a-z]|[0-9][0-9] [0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]");
+                foreach (var res in matchCollection)
+                {
+                    Console.WriteLine(res);
+                }
+
+
+            }
+        }
+    }
+
+
